5b8789c532fa403b8a1018cad6016a3cdef6c6fa
soniadila <99525698+soniadila@users.noreply.github.com>
2022-02-27 07:39:53 +0700

    projek pertama

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 26/02/2022
+ * Time: 10:57
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace BelajarCSHARP
+{
+	class ProgramHW
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
